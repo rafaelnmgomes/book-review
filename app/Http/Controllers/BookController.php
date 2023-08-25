09d1e68fb47b3a52ce95573ac42c55b565324765
Rafael Gomes <maciel.rafael1@gmail.com>
2023-08-25 08:34:47 -0400

    complete rate limiter

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -7,9 +7,6 @@ use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index(Request $request)
     {
         $title = $request->input('title');
@@ -32,25 +29,6 @@ class BookController extends Controller
         return view('books.index', compact('books'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
     public function show(int $id)
     {
         $cacheKey = 'book:' . $id;
@@ -64,29 +42,5 @@ class BookController extends Controller
         );
 
         return view('books.show', ['book' => $book]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\ReviewController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +20,6 @@ Route::get('/', function () {
 });
 
 Route::resource('books', BookController::class);
+
+Route::resource('books.reviews', ReviewController::class)
+    ->scoped(['review' => 'book']);
